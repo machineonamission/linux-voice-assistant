7ecf0dc772f2e44ef98a0ff8dda5cd62e57d10d4
Melody <machineonamission@gmail.com>
2026-04-22 09:31:16 -0500

    Fix syntax error in entity.py after merge

--- linux_voice_assistant/entity.py
+++ linux_voice_assistant/entity.py
@@ -234,7 +234,6 @@ class MediaPlayerEntity(ESPHomeEntity):
                         lambda: self._safe_send_state(MediaPlayerState.PAUSED),
                         done_callback,
                     ),
-                    done_callback=lambda: call_all(self.music_player.resume, done_callback),
                 )
             else:
                 # Nothing was playing, just announce then go idle
